97bf2105986b0172ab1ad657795b754e0750fe34
Evalin0316 <59949752+Evalin0316@users.noreply.github.com>
2021-05-19 20:27:22 +0800

    Initial commit

---
+++ login.js
@@ -0,0 +1,36 @@
+const url = 'https://vue3-course-api.hexschool.io';
+const path = 'ear077';
+
+
+const usernameInput = document.querySelector('#username')
+const passwordInput = document.querySelector('#password')
+const loginBtn = document.querySelector('#login')
+loginBtn.addEventListener('click',login);
+
+function login(e){
+    e.preventDefault();
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    const data = {
+        username,
+        password,
+    }
+    
+    console.log(data);
+    axios.post(`${url}/admin/signin`, data)
+        .then((res) => {
+        console.log(res);
+        if(res.data.success){
+            // const token = res.data.token;
+            // const expired =  res.data.expired;
+            const { token, expired } = res.data;
+            console.log(token,expired);
+            document.cookie = `hexToken=${token};expires=${new Date(expired)}; path=/`;
+            window.location.reload();
+        }
+    }).catch(()=>{
+        console.log('error');
+    });
+}
+
